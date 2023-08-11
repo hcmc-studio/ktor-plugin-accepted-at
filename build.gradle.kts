3d62a247300cce5e6cfcbee77f9fce4a5e1dbad8
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-11 23:25:30 +0900

    [Script push]

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "studio.hcmc"
-version = "0.0.6"
+version = "0.0.7"
 
 repositories {
     mavenCentral()
@@ -19,7 +19,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "studio.hcmc"
             artifactId = "ktor-plugin-accepted-at"
-            version = "0.0.6"
+            version = "0.0.7"
             from(components["java"])
         }
     }
